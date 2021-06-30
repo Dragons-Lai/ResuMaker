8e92b6deb2de0d091f6e430a34b203ebde8fb5ac
imfulee <imfulee@gmail.com>
2021-06-30 20:01:36 +0800

    change image on chunk

--- frontend/src/features/resume/Sidebar.js
+++ frontend/src/features/resume/Sidebar.js
@@ -1,5 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { insertChunk, sidebarSwitch, selectCurrentChunkId } from "./resumeSlice";
+import bpchunk1Img from "../../img/sidebar/bpchunk1.png";
+import bpchunk2Img from "../../img/sidebar/bpchunk2.png";
+import titleImg from "../../img/sidebar/title.png";
+import contentImg from "../../img/sidebar/content.png";
+import lineImg from "../../img/sidebar/line.png";
 import { Button, Row, Card } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 
@@ -14,44 +19,16 @@ export default function Sidebar() {
       <Row>
         <Button type="text" icon={<CloseOutlined />} onClick={() => dispatch(sidebarSwitch())} block />
       </Row>
-      {/* <Row>
-        <Card
-          hoverable
-          cover={<img alt="type-1" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          onClick={() => {
-            dispatch(insertChunk(currentChunkId, "type1", "down"));
-            dispatch(sidebarSwitch());
-          }}
-          className="sidebarChunkType1"
-          style={{ width: 180, margin: 10 }}
-        >
-          <Meta description="Chunk Type1" />
-        </Card>
-      </Row> */}
       <Row>
         <Card
           hoverable
-          cover={<img alt="type-1" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          onClick={() => {
-            dispatch(insertChunk(currentChunkId, "_1", "down"));
-            dispatch(sidebarSwitch());
-          }}
-          className="sidebarChunkType1"
-          style={{ width: 180, margin: 10 }}
-        >
-          <Meta description="個人資訊" />
-        </Card>
-      </Row>
-      <Row>
-        <Card
-          hoverable
-          cover={<img alt="type-2" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          cover={<img alt="type-2" src={bpchunk1Img} />}
           onClick={() => {
             dispatch(insertChunk(currentChunkId, "bpChunk_1", "down"));
             dispatch(sidebarSwitch());
           }}
           className="sidebarChunkType2"
-          style={{ width: 180, margin: 10 }}
+          style={{ width: 900, margin: 10 }}
         >
           <Meta description="列點式（樣式一）" />
         </Card>
@@ -59,7 +36,7 @@ export default function Sidebar() {
       <Row>
         <Card
           hoverable
-          cover={<img alt="type-3" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          cover={<img alt="type-3" src={bpchunk2Img} />}
           onClick={() => {
             dispatch(insertChunk(currentChunkId, "bpChunk_2", "down"));
             dispatch(sidebarSwitch());
@@ -73,7 +50,7 @@ export default function Sidebar() {
       <Row>
         <Card
           hoverable
-          cover={<img alt="type-4" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          cover={<img alt="type-4" src={titleImg} />}
           onClick={() => {
             dispatch(insertChunk(currentChunkId, "mtChunk_1", "down"));
             dispatch(sidebarSwitch());
@@ -87,7 +64,7 @@ export default function Sidebar() {
       <Row>
         <Card
           hoverable
-          cover={<img alt="type-5" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          cover={<img alt="type-5" src={contentImg} />}
           onClick={() => {
             dispatch(insertChunk(currentChunkId, "mcChunk_1", "down"));
             dispatch(sidebarSwitch());
@@ -101,7 +78,7 @@ export default function Sidebar() {
       <Row>
         <Card
           hoverable
-          cover={<img alt="type-6" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          cover={<img alt="type-6" src={lineImg} />}
           onClick={() => {
             dispatch(insertChunk(currentChunkId, "lineChunk_1", "down"));
             dispatch(sidebarSwitch());
