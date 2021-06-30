71e402d4c09878b1d83d10dbb007a848367b34c1
imfulee <imfulee@gmail.com>
2021-06-30 15:59:32 +0800

    fixed avatar name centering issue

--- frontend/src/features/resume/ResumeHeader.js
+++ frontend/src/features/resume/ResumeHeader.js
@@ -10,7 +10,6 @@ import { VIEW_MODE, EDIT_MODE } from "./config";
 import { logout } from "../homePage/api";
 import { selectChunkIdList, selectChangeRecord, clearChangeRecord, selectSidebarStatus } from "./resumeSlice";
 
-
 const { Header } = Layout;
 const { Option } = Select;
 
@@ -22,94 +21,108 @@ export default function ResumeBody({ mode, setMode }) {
   // to make the sidebar collapsible
   const openSidebar = useSelector(selectSidebarStatus);
 
-  const [userName, setUserName] = useState("")
-  const [shareModelVisibility, setShareModelVisibility] = useState(false)
-  const [sharable, updateSharable] = useState(false)
-  const [sharableUrlSuffix, setSharableUrlSuffix] = useState("")
+  const [userName, setUserName] = useState("");
+  const [shareModelVisibility, setShareModelVisibility] = useState(false);
+  const [sharable, updateSharable] = useState(false);
+  const [sharableUrlSuffix, setSharableUrlSuffix] = useState("");
 
   useEffect(() => {
     getUserName().then((res) => {
-      setUserName(res)
-    })
+      setUserName(res);
+    });
     getSharable().then((res) => {
-      console.log("aaaaaaaaaaa", res)
-      updateSharable(res.sharable)
-      setSharableUrlSuffix(res.url_suffix)
-    })
-  }, [])
+      console.log("aaaaaaaaaaa", res);
+      updateSharable(res.sharable);
+      setSharableUrlSuffix(res.url_suffix);
+    });
+  }, []);
 
-  const modeOppsite = {}
-  modeOppsite[EDIT_MODE] = VIEW_MODE
-  modeOppsite[VIEW_MODE] = EDIT_MODE
+  const modeOppsite = {};
+  modeOppsite[EDIT_MODE] = VIEW_MODE;
+  modeOppsite[VIEW_MODE] = EDIT_MODE;
 
-  const changeModeText = {}
-  changeModeText[EDIT_MODE] = "View"
-  changeModeText[VIEW_MODE] = "Edit"
+  const changeModeText = {};
+  changeModeText[EDIT_MODE] = "View";
+  changeModeText[VIEW_MODE] = "Edit";
 
-  return (<>
-    <Header>
-      <Row>
-        <Col span={8}>
-          <Space size="small">
-            <Link to="/">
-              <Button className=" navbar-button-group home-button" icon={<HomeTwoTone />} />
-            </Link>
-            <Button
-              className="save-button"
-              type="primary"
-              icon={<SaveOutlined />}
-              onClick={() => {
-                saveChunk(chunkIdList, changeRecord).then(() => dispatch(clearChangeRecord()));
+  return (
+    <>
+      <Header>
+        <Row>
+          <Col span={8}>
+            <Space size="small">
+              <Link to="/">
+                <Button className=" navbar-button-group home-button" icon={<HomeTwoTone />} />
+              </Link>
+              <Button
+                className="save-button"
+                type="primary"
+                icon={<SaveOutlined />}
+                onClick={() => {
+                  saveChunk(chunkIdList, changeRecord).then(() => dispatch(clearChangeRecord()));
+                }}
+              >
+                Save
+              </Button>
+              <Button type="primary" onClick={() => setMode(modeOppsite[mode])}>
+                {changeModeText[mode]}
+              </Button>
+              <Button type="primary" onClick={() => setShareModelVisibility(true)}>
+                Share
+              </Button>
+            </Space>
+          </Col>
+          <Col span={12} />
+          <Col span={1}>
+            <Avatar alt={userName} shape="square">
+              {userName}
+            </Avatar>
+          </Col>
+          <Col span={1}>
+            <div className="user-name-row">
+              <p className="user-name">{userName}</p>
+            </div>
+          </Col>
+          <Col span={2}>
+            <Button className="logout-button" type="primary" onClick={() => logout()}>
+              Logout
+            </Button>
+          </Col>
+        </Row>
+      </Header>
+      <Modal
+        title={
+          <Space align="center">
+            ShareLink
+            <Select
+              defaultValue={sharable === true ? "sharable" : "unsharable"}
+              style={{ width: 120 }}
+              onChange={(value) => {
+                updateSharable(value);
               }}
             >
-              Save
-            </Button>
-            <Button type="primary" onClick={() => setMode(modeOppsite[mode])}>
-              {changeModeText[mode]}
-            </Button>
-            <Button type="primary" onClick={() => setShareModelVisibility(true)}>
-              Share
-            </Button>
+              <Option value={true} label="sharable">
+                shared
+              </Option>
+              <Option value={false} label="unsharable">
+                unshared
+              </Option>
+            </Select>
           </Space>
-        </Col>
-        <Col span={12} />
-        <Col span={2}>
-          <div className="user-name-row">
-            <Avatar shape="square" size="small" icon={<UserOutlined />} />
-            <p className="user-name">{userName}</p>
-          </div>
-        </Col>
-        <Col span={2}>
-          <Button className="logout-button" type="primary" onClick={() => logout()}>
-            Logout
-          </Button>
-        </Col>
-      </Row>
-    </Header>
-    < Modal
-      title={
-        <Space align="center">
-          ShareLink
-          <Select defaultValue={sharable === true ? "sharable" : "unsharable"} style={{ width: 120 }} onChange={(value) => {
-            updateSharable(value)
-          }}>
-            <Option value={true} label="sharable">shared</Option>
-            <Option value={false} label="unsharable">unshared</Option>
-          </Select>
-        </Space>
-      }
-      visible={shareModelVisibility}
-      onOk={() => {
-        setSharable(sharable)
-        setShareModelVisibility(false)
-      }}
-      onCancel={() => {
-        setShareModelVisibility(false)
-      }}
-      okText="Set"
-      cancelText="Cancel"
-    >
-      <p>{`http://localhost:3000/viewResume/${sharableUrlSuffix}`}</p>
-    </Modal >
-  </>)
+        }
+        visible={shareModelVisibility}
+        onOk={() => {
+          setSharable(sharable);
+          setShareModelVisibility(false);
+        }}
+        onCancel={() => {
+          setShareModelVisibility(false);
+        }}
+        okText="Set"
+        cancelText="Cancel"
+      >
+        <p>{`http://localhost:3000/viewResume/${sharableUrlSuffix}`}</p>
+      </Modal>
+    </>
+  );
 }
